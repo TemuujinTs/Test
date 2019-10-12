7ce854bf540d655d4e93777954a9e996edb03520
Temka <temka.fir90@gmail.com>
2019-10-12 15:07:53 +0800

    School class

---
+++ src/school/management/system/School.java
@@ -0,0 +1,69 @@
+package school.management.system;
+
+import java.util.List;
+
+/**
+ * Many teachers, many students.
+ */
+public class School {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
+    public School(List<Teacher> teachers, List<Student> students) {
+        this.teachers    = teachers;
+        this.students    = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent  = 0;
+    }
+
+    /**
+     *
+     * @return the list of teachers in the school.
+     */
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    /**
+     * Adds a teacher to the school
+     * @param teacher the teacher to be added.
+     */
+    public void addTeacher(Teacher teacher) {
+       teachers.add(teacher);
+    }
+
+    /**
+     *
+     * @return the list of students in the school.
+     */
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * Adds a student to the school
+     * @param student the student to be added.
+     */
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public void setTotalMoneyEarned(int totalMoneyEarned) {
+        this.totalMoneyEarned = totalMoneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public void setTotalMoneySpent(int totalMoneySpent) {
+        this.totalMoneySpent = totalMoneySpent;
+    }
+}
